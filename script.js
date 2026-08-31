6bc76c589e1cddd0bb241d0da3209197684a13ec
Vitrio14 <info.antoniotroiani@gmail.com>
2026-08-31 15:12:20 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -711,13 +711,13 @@ function initDatabaseListeners() {
     });
 }
 
-// // Protezione
-// document.addEventListener('contextmenu', event => event.preventDefault());
-// document.onkeydown = function(e) {
-//     if (e.keyCode == 123) return false;
-//     if (e.ctrlKey && e.shiftKey && (e.keyCode == 'I'.charCodeAt(0) || e.keyCode == 'C'.charCodeAt(0) || e.keyCode == 'J'.charCodeAt(0))) return false;
-//     if (e.ctrlKey && e.keyCode == 'U'.charCodeAt(0)) return false;
-// };
+// Protezione
+document.addEventListener('contextmenu', event => event.preventDefault());
+document.onkeydown = function(e) {
+    if (e.keyCode == 123) return false;
+    if (e.ctrlKey && e.shiftKey && (e.keyCode == 'I'.charCodeAt(0) || e.keyCode == 'C'.charCodeAt(0) || e.keyCode == 'J'.charCodeAt(0))) return false;
+    if (e.ctrlKey && e.keyCode == 'U'.charCodeAt(0)) return false;
+};
 
 // --- STASH DROPDOWNS ---
 function renderStashDropdowns() {
